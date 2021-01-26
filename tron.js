9e468a18350fe14e2545f4e6d27563c6cb20036c
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2021-01-25 23:19:12 -0800

    functioning trail added, collision added, fills in gaps from step being larger

--- tron.js
+++ tron.js
@@ -7,10 +7,11 @@ const board_background = "white";
 const snake_col = 'lightblue';
 const snake_border = 'darkblue';
 let snake = { x: 0, y: 200 };
-let lastPos = { x: 0, y: 0 };
+let prevTrail = { x: 0, y: 0 };
+let currTrail = { x: 0, y: 0 };
 const playerSize = 10;
 const step = 3;
-const trailSize = 2;
+const trailSize = 1;
 
 
 // True if changing direction
@@ -19,6 +20,15 @@ let changing_direction = false;
 let dx = step;
 // Vertical velocity
 let dy = 0;
+
+
+let goingUp = dy === -step;
+let goingDown = dy === step;
+let goingRight = dx === step;
+let goingLeft = dx === -step;
+
+//default moving right, front hitbox coords are top right and bottom right of player
+let hitbox = [{ x: snake.x, y: snake.y }, { x: snake.x+playerSize, y: snake.y+playerSize }];
 
 const snakeboard = document.getElementById("gameCanvas");
 snakeboard.width = window.innerWidth * .9;
@@ -33,8 +43,6 @@ for(var row = 0; row < snakeboard.height; row++){
     trailMap[row][col] = 0;
   }
 }
-console.log(snakeboard.width, snakeboard.height);
-console.log(trailMap)
 // Start game
 main();
 
@@ -53,7 +61,7 @@ function main() {
     drawTrail();
     // Call main again
     main();
-  }, 10)
+  }, 20)
 }
 
 function clearCanvas() {
@@ -67,15 +75,7 @@ function clearCanvas() {
     snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
   }
 
-// function drawSnakePart(snakePart) 
-// {  
-//   snakeboard_ctx.fillStyle = snake_col;  
-//   snakeboard_ctx.strokestyle = snake_border;
-//   snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);  
-//   snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-// }
- 
-/*Function that prints the parts*/
+
 function drawSnake() 
 {  
   snakeboard_ctx.fillStyle = snake_col;  
@@ -83,52 +83,107 @@ function drawSnake()
   snakeboard_ctx.fillRect(snake.x, snake.y, playerSize, playerSize);  
   snakeboard_ctx.strokeRect(snake.x, snake.y, playerSize, playerSize);
 
-  // snake.forEach(drawSnakePart);
 }
 
 function drawTrail(lastX, lastY) {
   snakeboard_ctx.fillStyle = 'red';  
-  snakeboard_ctx.strokestyle = 'pink';
-  snakeboard_ctx.fillRect(lastPos.x, lastPos.y, 1, 1);  
-  snakeboard_ctx.strokeRect(lastPos.x, lastPos.y, 1, 1);
+  // snakeboard_ctx.strokeStyle = 'pink';
+  snakeboard_ctx.fillRect(prevTrail.x, prevTrail.y, 1, 1);  
+  snakeboard_ctx.strokeRect(prevTrail.x, prevTrail.y, 1, 1);
 
   snakeboard_ctx.fillStyle = 'green';
 
   let trailPoints = [];
   for(var row = 0; row < trailMap.length; row++){
     for(var col = 0; col < trailMap[0].length; col++){
+      //updates immune trail to not be immune anymore, didnt wanna make an extra double for loop
+      if(trailMap[row][col] == 2){
+        let immuneSpot = inHitBox(row, col);
+        if(!immuneSpot){
+          trailMap[row][col] = 1;
+        }
+      }
       if(trailMap[row][col] == 1){
         snakeboard_ctx.fillRect(col, row, trailSize, trailSize);  
       }
     }
   }
+}
 
-
-  
-
-
+function inHitBox(row, col) {
+  for(let i = hitbox[0].y; i <= hitbox[1].y; i++){
+    for(let j = hitbox[0].x; j <= hitbox[1].x; j++){
+      //if row and col are in hitbox, they're immune
+      if(row == i && col == j){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 function move_snake() {
-    lastPos.x = (snake.x+(snake.x+playerSize))/2;
-    lastPos.y = (snake.y+(snake.y+playerSize))/2;
-    trailMap[lastPos.y][lastPos.x] = 1;
+    
+    // centerLastPos.x = (snake.x+(snake.x+playerSize))/2;
+    // centerLastPos.y = (snake.y+(snake.y+playerSize))/2;
+    prevTrail.x = currTrail.x;
+    prevTrail.y = currTrail.y;
+    // trailMap[centerLastPos.y][centerLastPos.x] = 1;
 
     snake.x = snake.x + dx;
     snake.y = snake.y + dy;
 
+    currTrail.x = (snake.x+(snake.x+playerSize))/2;
+    currTrail.y = (snake.y+(snake.y+playerSize))/2;
+
+    // trailMap[centerCurrPos.y][centerCurrPos.x] = 1;
+
+    goingUp = dy === -step;
+    goingDown = dy === step;
+    goingRight = dx === step;
+    goingLeft = dx === -step;
+
+    hitbox[0] = { x: snake.x, y: snake.y };
+    hitbox[1] = { x: snake.x+playerSize, y: snake.y+playerSize };
+
+    if(goingUp){
+      //fill in trail behind you
+      for(let i=prevTrail.y; i>currTrail.y; i--){
+        trailMap[i][currTrail.x] = 2;
+      }
+    }
+
+    else if(goingDown){
+      //fill in trail behind you
+      for(let i=prevTrail.y; i<currTrail.y; i++){
+        trailMap[i][currTrail.x] = 2;
+      }
+    }
+
+    else if(goingLeft){
+      //fill in trail behind you
+      for(let i=prevTrail.x; i>currTrail.x; i--){
+        trailMap[currTrail.y][i] = 2;
+      }
+    }
+
+    else if(goingRight){
+      //fill in trail behind you
+      for(let i=prevTrail.x; i<currTrail.x; i++){
+        trailMap[currTrail.y][i] = 2;
+      }
+    }
 
 }
 
 function has_game_ended() {
-    // for (let i = 4; i < snake.length; i++) {
-    //   if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
-    // }
+
     const hitLeftWall = snake.x < 0;
-    const hitRightWall = snake.x > snakeboard.width - playerSize;
+    const hitRightWall = snake.x >= snakeboard.width - playerSize;
     const hitToptWall = snake.y < 0;
-    const hitBottomWall = snake.y > snakeboard.height - playerSize;
-    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
+    const hitBottomWall = snake.y >= snakeboard.height - playerSize;
+
+    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall || trailHit()
 }
 
 function change_direction(event) {
@@ -166,3 +221,20 @@ function change_direction(event) {
       dy = step;
     }
 }
+
+//return true if player has hit trail
+//false if not
+function trailHit() {
+
+  for(let i = hitbox[0].y; i <= hitbox[1].y; i++){
+    for(let j = hitbox[0].x; j <= hitbox[1].x; j++){
+      if(trailMap[i][j] == 1){
+        return true;
+      }
+    }
+  }
+
+
+  return false;
+}
+
